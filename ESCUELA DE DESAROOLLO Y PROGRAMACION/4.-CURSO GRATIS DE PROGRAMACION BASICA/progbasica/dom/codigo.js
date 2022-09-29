8cf2d2145d0bd66e03e4b15238c129d8897b7796
juanpa7799 <garsa779999@gmail.com>
2022-09-29 14:43:00 -0500

    SUbiendo todos los demas archivos de las demas escuelas

---
+++ ESCUELA DE DESAROOLLO Y PROGRAMACION/4.-CURSO GRATIS DE PROGRAMACION BASICA/progbasica/dom/codigo.js
@@ -0,0 +1,5 @@
+var ruta = window.location; //No esta en parentesis por que no es una funcion, es una variable interna del objeto window.
+//cuando una variable es parte de un objeto, se le llama "atributo o propiedad". location es un atributo o propiedad del window.
+console.log(ruta); //Nos ayuda a ver todo desde su origen y su desglosamiento.
+document.write("Estás en: " + ruta); //document es parte del objet model, es un objeto con todo el archivo, por lo que .write, invoca donde escribir en el documento.
+  //cuando las funciones son parte de un objeto se llaman "metodos" (parte de un objeto).
